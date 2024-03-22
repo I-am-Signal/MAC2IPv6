1a4964530c416096da125b3fb0da75d674743014
Dylan_Carder <123211944+I-am-Signal@users.noreply.github.com>
2024-03-22 17:29:13 -0400

    Create Source Files

---
+++ Source_FIles/Convert.cs
@@ -0,0 +1,114 @@
+/*
+ *  MAC2IPv6
+ *  Command line conversion script to convert a MAC address to an IPv6 address
+ *  Written by Dylan Carder
+ *  March 22nd, 2024
+ *  Idea suggested by Jake B at Kennesaw State University
+ *  https://discord.com/channels/730029345954988082/1140806104402640917/1220599398204047420
+ *  Convert.cs
+*/
+
+namespace ConvertMACToIPv6
+{
+    public static class Converter
+    {
+        /// <summary>
+        /// Returns true if the string provided is in MAC address format
+        /// </summary>
+        /// <param name="MACAddress"></param>
+        /// <returns></returns>
+        public static bool CheckMACAddrFormat(string? MACAddress)
+        {
+            // used to check for delimiters
+            char? Delimiter = null;
+
+            // format verification
+            if (MACAddress == null || (MACAddress.Length != 12 && MACAddress.Length != 17))
+                return false;
+
+            if (MACAddress.Length == 17)
+            {
+                Delimiter = MACAddress[2];
+            }
+
+            else if (MACAddress.Length != 12)
+                return false;
+
+            MACAddress.ToLower();
+
+            for (int i = 0; i < MACAddress.Length; i++)
+            {
+                // checking delimiters when present
+                if ((i + 1) % 3 == 0 && Delimiter != null)
+                {
+                    if (MACAddress[i] != Delimiter)
+                        return false;
+                    continue;
+                }
+
+                // verifying hexadecimal characters
+                if (char.IsDigit(MACAddress[i]))
+                    continue;
+
+                if (MACAddress[i] < 'a' || MACAddress[i] > 'f')
+                    return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Returns the converted MACAddress string in IPv6 format
+        /// </summary>
+        /// <param name="MACAddress"></param>
+        /// <returns></returns>
+        public static string MACAddrToIPv6(string MACAddress)
+        {
+            // remove delimiters to ease calculation
+            if (MACAddress.Length == 17)
+            {
+                string Temp = MACAddress.Substring(0, 2);
+                Temp += MACAddress.Substring(3, 2);
+                Temp += MACAddress.Substring(6, 2);
+                Temp += MACAddress.Substring(9, 2);
+                Temp += MACAddress.Substring(12, 2);
+                Temp += MACAddress.Substring(15, 2);
+                MACAddress = Temp;
+            }
+
+            /******************************************************************************
+            *    Title: C# how convert large HEX string to binary
+            *    Author: Guffa
+            *    Date: 2011
+            *    Availability: https://stackoverflow.com/questions/6617284/c-sharp-how-convert-large-hex-string-to-binaryhttps://stackoverflow.com/questions/6617284/c-sharp-how-convert-large-hex-string-to-binary
+            *******************************************************************************/
+            // convert first octet to binary
+            string BinaryString = String.Join(String.Empty, MACAddress.Substring(0, 2).Select(
+                c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')));
+
+            // invert seventh bit
+            BinaryString = BinaryString[6] == '0' ?
+                BinaryString.Substring(0, 6) + '1' + BinaryString[7]:
+                BinaryString.Substring(0, 6) + '0' + BinaryString[7];
+
+            // replace first octet with newly converted one
+            MACAddress = Convert.ToInt32(BinaryString, 2).ToString("X").ToLower() +
+                MACAddress.Substring(2, 10);
+
+            // add 'fffe' to the middle of the string
+            MACAddress = MACAddress.Substring(0, MACAddress.Length / 2) + "fffe" +
+                MACAddress.Substring(MACAddress.Length / 2, MACAddress.Length / 2);
+
+            // adjust formatting to IPv6
+            string IPv6 = MACAddress.Substring(0, 4) + ":";
+            IPv6 += MACAddress.Substring(4, 4) + ":";
+            IPv6 += MACAddress.Substring(8, 4) + ":";
+            IPv6 += MACAddress.Substring(12, 4);
+
+
+            // add 'fe80::' to the beginning of the string
+            IPv6 = "fe80::" + IPv6;
+
+            return IPv6;
+        }
+    }
+}
---
+++ Source_FIles/MainClass.cs
@@ -0,0 +1,55 @@
+﻿/*
+ *  MAC2IPv6
+ *  Command line conversion script to convert a MAC address to an IPv6 address
+ *  Written by Dylan Carder
+ *  March 22nd, 2024
+ *  Idea suggested by Jake B at Kennesaw State University
+ *  https://discord.com/channels/730029345954988082/1140806104402640917/1220599398204047420
+ *  MainClass.cs
+*/
+
+namespace ConvertMACToIPv6
+{
+    public class MainClass
+    {
+        /// <summary>
+        /// Displays the IPv6 address to the console if the address is in the correct format
+        /// </summary>
+        /// <param name="Address"></param>
+        /// <returns></returns>
+        public static void CheckThenConvert(string? Address)
+        {
+            // correct format
+            if (Converter.CheckMACAddrFormat(Address))
+            {
+                Console.WriteLine(" IPv6 Address: " + Converter.MACAddrToIPv6(Address + ""));
+            }
+
+            // incorrect format
+            else
+            {
+                Console.WriteLine(" MAC Address is not in the correct format.");
+                Console.WriteLine(" MAC Address formats:");
+                Console.WriteLine("  1) XX:XX:XX:XX:XX:XX");
+                Console.WriteLine("  2) XXXXXXXXXXXX");
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("[MAC Address to IPv6 Address]");
+            Console.Write(" MAC Address: ");
+
+            // using the command line initialization
+            if (args != null && args.Length == 1)
+            {
+                Console.WriteLine(args[0]);
+                CheckThenConvert(args[0]);
+                return;
+            }
+
+            // using the command line menu
+            CheckThenConvert(Console.ReadLine());
+        }
+    }
+}
